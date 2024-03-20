fff1d46bed8f8c9ed97dd00891429bba0819d095
MWOR <oliveiramisael20@gmail.com>
2024-03-20 16:14:10 -0300

    feat:added all archive
    added all archive, error

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,25 @@
+"""
+URL configuration for core project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.urls import path
-from weather.views import WeatherView
+from weather.views import WeatherView, WeatherGenerate, WeatherReset
 
 urlpatterns = [
-    path('', WeatherView.as_view()),
+    path('', WeatherView.as_view(), name='Weather View'),
+    path('generate', WeatherGenerate.as_view(), name='Weather Generate'),
+    path('reset', WeatherReset.as_view(), name='Weather Reset'),
 
 ]
